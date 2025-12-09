a67c02ba1e145fd198c3173bf7851f967d764cfe
Baswaraj <baswaraj.yedrami@gmail.com>
2025-12-09 07:34:58 +0530

    commit1

---
+++ src/main/java/com/wipro/Calculator.java
@@ -0,0 +1,20 @@
+package com.wipro;
+
+public class Calculator {
+	
+	public static int addition(int num1,int num2) {
+		return num1+num2;
+	}
+	
+	public static int subtraction(int num1,int num2) {
+		return num1-num2;
+	}
+	
+	public static int multiplication(int num1,int num2) {
+		return num1*num2;
+	}
+	public static int division(int num1,int num2) {
+		return num1/num2;
+	}
+
+}
---
+++ src/test/java/com/wipro/test/CalculatorTest.java
@@ -0,0 +1,31 @@
+package com.wipro.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.wipro.Calculator;
+
+public class CalculatorTest {
+
+	@Test
+	public void testAddition() {
+		assertEquals(10,Calculator.addition(5, 5));
+	}
+
+	@Test
+	public void testSubtraction() {
+		assertEquals(10,Calculator.subtraction(20, 10));
+	}
+
+	@Test
+	public void testMultiplication() {
+		assertEquals(25,Calculator.multiplication(5, 5));
+	}
+
+	@Test
+	public void testDivision() {
+		assertEquals(1,Calculator.division(5, 5));
+	}
+
+}
